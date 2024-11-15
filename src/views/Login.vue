7c764f8d76ca5f0832e4ef5862b8d522259d5741
John Crossman <johncrossman@berkeley.edu>
2024-11-14 16:55:15 -0800

    DAMIEN-663, fix /login page UI

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,85 +1,84 @@
 <template>
-  <v-app>
+  <v-app class="background-lecture-hall">
     <Snackbar />
-    <v-container class="background-lecture-hall" fill-height fluid>
-      <v-main>
-        <v-card
-          class="mx-auto px-8 py-6 frosted accent-border accent--text"
-          max-width="600"
-          outlined
-        >
-          <div class="text-center">
-            <h1 id="page-title">
-              <strong>Welcome to Course Evaluations</strong>
-              {{ contextStore.config.currentTermName }}
-            </h1>
-          </div>
-          <v-card-actions class="px-16 pt-12 d-flex flex-column">
-            <v-btn
-              id="log-in"
-              aria-label="Log in to Course Evaluations. (You will be sent to CalNet login page.)"
-              block
-              color="accent"
-              large
-              @click.stop="logIn"
-              @keypress.enter.prevent.stop="logIn"
-            >
-              Sign In
-              <v-icon class="pl-2" :icon="mdiArrowRightCircleOutline" />
-            </v-btn>
-            <DevAuth v-if="contextStore.config.devAuthEnabled" />
-          </v-card-actions>
-        </v-card>
-      </v-main>
-    </v-container>
+    <v-main class="align-center d-flex flex-column h-100">
+      <div class="my-auto">
+        <v-slide-y-transition>
+          <v-card v-show="showCard" class="accent-border frosted mx-auto pb-9 pt-6 px-10" outlined>
+            <v-card-text class="text-center">
+              <h1 id="page-title" class="text-accent">
+                <strong>Welcome to Course Evaluations</strong>
+                {{ config.currentTermName }}
+              </h1>
+            </v-card-text>
+            <v-card-actions class="d-flex flex-column">
+              <v-btn
+                id="log-in"
+                aria-label="Log in to Course Evaluations. (You will be sent to CalNet login page.)"
+                class="w-100"
+                color="accent"
+                size="large"
+                variant="flat"
+                @click.stop="logIn"
+              >
+                Sign In
+                <div class="pl-2">
+                  <v-icon :icon="mdiArrowRightCircleOutline" />
+                </div>
+              </v-btn>
+              <DevAuth v-if="config.devAuthEnabled" class="mb-2 mt-8" />
+            </v-card-actions>
+          </v-card>
+        </v-slide-y-transition>
+      </div>
+    </v-main>
   </v-app>
 </template>
 
 <script setup>
-import {useContextStore} from '@/stores/context'
-const contextStore = useContextStore()
-</script>
-
-<script>
 import DevAuth from '@/components/admin/DevAuth'
 import Snackbar from '@/components/util/Snackbar'
 import {alertScreenReader, putFocusNextTick} from '@/lib/utils'
-import {get} from 'lodash'
 import {getCasLoginURL} from '@/api/auth'
 import {mdiArrowRightCircleOutline} from '@mdi/js'
+import {onMounted, ref} from 'vue'
+import {useContextStore} from '@/stores/context'
+import {useRoute} from 'vue-router'
 
-export default {
-  name: 'Login',
-  components: {
-    DevAuth,
-    Snackbar
-  },
-  data: () => ({
-    mdiArrowRightCircleOutline
-  }),
-  created() {
-    putFocusNextTick('page-title')
-    const error = get(this.$route, 'query.error')
-    if (error) {
-      useContextStore().snackbarReportError(error)
-    } else {
-      alertScreenReader(`Welcome to Course Evaluations - ${useContextStore().config.currentTermName}. Please log in.`)
-    }
-  },
-  methods: {
-    logIn() {
-      getCasLoginURL().then(data => window.location.href = data.casLoginUrl)
-    }
+const contextStore = useContextStore()
+const config = contextStore.config
+const route = useRoute()
+const showCard = ref(false)
+
+onMounted(() => {
+  setTimeout(
+    () => {
+      showCard.value = true
+      alertScreenReader('Page not found')
+    },
+    100
+  )
+  putFocusNextTick('page-title')
+  const error = route.query.error
+  if (error) {
+    contextStore.snackbarReportError(error)
+  } else {
+    alertScreenReader(`Welcome to Course Evaluations - ${config.currentTermName}. Please log in.`)
   }
-}
+})
+
+const logIn = () => getCasLoginURL().then(data => window.location.href = data.casLoginUrl)
 </script>
 
 <style scoped>
 .accent-border {
   border: 1px solid #F04A00 !important;
 }
+.page-container {
+  height: calc(100vh - 0px);
+}
 .background-lecture-hall {
-  background: url('~@/assets/lecture_hall_background.jpg') no-repeat center;
+  background: url('@/assets/lecture_hall_background.jpg') no-repeat center center fixed;
   -webkit-background-size: cover;
   -moz-background-size: cover;
   -o-background-size: cover;
